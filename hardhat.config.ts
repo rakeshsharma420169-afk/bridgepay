1c49634c514d56a334b285e99ed5a3c8edcc8dde
rakeshsharma420169-afk <rakeshsharma420169@gmail.com>
2025-11-23 16:07:18 +0530

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,20 +2,11 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: {
-    version: "0.8.19",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
-      }
-    }
-  },
+  solidity: "0.8.19",
   networks: {
-    hardhat: {
-      chainId: 31337
+    sepolia: {
+      url: "https://sepolia.infura.io/v3/YOUR_INFURA_KEY",
+      accounts: ["0xAf8a9Db8d6B73E24bFF24cD34Ce4AC580486E854"]
     }
   }
 };
-
-export default config;
